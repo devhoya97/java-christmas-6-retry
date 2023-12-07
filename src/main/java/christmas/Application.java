1d4a747067408c504f7f61e5d6856817703ec590
devhoya97 <diajbh123@gmail.com>
2023-12-07 23:25:25 +0900

    feat: 할인 전 총주문 금액을 출력하는 기능 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -7,8 +7,10 @@ import java.util.Map;
 public class Application {
     public static void main(String[] args) {
         Map<String, Integer> orderInput = new HashMap<>();
-        orderInput.put("해산물파스타", 2);
+        orderInput.put("티본스테이크", 1);
+        orderInput.put("바비큐립", 1);
         orderInput.put("초코케이크", 2);
+        orderInput.put("제로콜라", 1);
 
         Order order = new Order(orderInput);
         OutputView outputView = new OutputView();
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,11 +1,13 @@
 package christmas.view;
 
+import christmas.Event;
 import christmas.Order;
 import christmas.menu.Menu;
 import java.util.Map.Entry;
 
 public class OutputView {
     private static final String NEW_LINE = "\n";
+    private static final String DOUBLE_NEW_LINE = "\n\n";
     private static final String SPACE = " ";
 
     public void printOrder(Order order) {
@@ -16,6 +18,9 @@ public class OutputView {
             message.append(orderItem.getKey().getName()).append(SPACE)
                     .append(orderItem.getValue()).append("개").append(NEW_LINE);
         }
+
+        message.append(NEW_LINE).append("<할인 전 총주문 금액>").append(NEW_LINE)
+                .append(order.calculateTotalPrice()).append("원").append(NEW_LINE);
 
         System.out.println(message);
     }
