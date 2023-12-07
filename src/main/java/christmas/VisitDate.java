474fd968c729c4ce9a0badbb4a99a90d00df15c9
devhoya97 <diajbh123@gmail.com>
2023-12-07 19:56:32 +0900

    feat: 방문 날짜가 1~31 사이가 아니라면 예외 발생

--- src/main/java/christmas/VisitDate.java
+++ src/main/java/christmas/VisitDate.java
@@ -1,9 +1,21 @@
 package christmas;
 
+import static christmas.ChristmasConstants.INVALID_VISIT_DATE;
+
 public class VisitDate {
+    private static final int MIN_DATE = 1;
+    private static final int MAX_DATE = 31;
+    private final int date;
+
+    public VisitDate(int date) {
+        validateDecember(date);
+        this.date = date;
+    }
 
     private void validateDecember(int date) {
-
+        if (date < MIN_DATE || date > MAX_DATE) {
+            throw new IllegalArgumentException(INVALID_VISIT_DATE);
+        }
     }
 
     public boolean isInChristmasPromotion() {
