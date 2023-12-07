9a2d473a09a3fc1334273142a9d18c2dee74c7de
devhoya97 <diajbh123@gmail.com>
2023-12-07 19:11:58 +0900

    feat: 메뉴판의 메뉴들을 종합하는 기능 구현

--- src/test/java/christmas/Order.java
+++ src/test/java/christmas/Order.java
@@ -1,14 +1,46 @@
 package christmas;
 
+import christmas.menu.Appetizer;
+import christmas.menu.Dessert;
+import christmas.menu.Drink;
+import christmas.menu.Main;
 import christmas.menu.Menu;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Order {
-    private void validateTotalMenuNumber(Map<Menu, Integer> order) {
+
+    private final Map<Menu, Integer> order;
+
+    public Order(Map<String, Integer> order) {
+        validatePrepared(order);
+        validateTotalMenuNumber(order);
+
 
     }
 
-    private void validatePrepared(Map<Menu, Integer> order) {
+    private Menu findMenu(String name) {
+        List<Menu> preparedMenus = getPreparedMenus();
+        return null;
+    }
+
+    private List<Menu> getPreparedMenus() {
+        List<Menu> preparedMenus = new ArrayList<>();
+
+        preparedMenus.addAll(Appetizer.getPreparedMenus());
+        preparedMenus.addAll(Main.getPreparedMenus());
+        preparedMenus.addAll(Dessert.getPreparedMenus());
+        preparedMenus.addAll(Drink.getPreparedMenus());
+
+        return preparedMenus;
+    }
+
+    private void validatePrepared(Map<String, Integer> order) {
+
+    }
+
+    private void validateTotalMenuNumber(Map<String, Integer> order) {
 
     }
 
